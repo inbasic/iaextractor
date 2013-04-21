098ea0e27b1ed3f22765b7963b7f3b90868c47ab
Alejandro Rodriguez <otor3-otor2@yahoo.com.ar>
2013-04-20 23:15:56 -0300

    bug 28 follow up 2

--- src/lib/main.js
+++ src/lib/main.js
@@ -495,7 +495,13 @@ var get = function (videoID, listener, fIndex) {
       return;
     }
     //Creating temporary files
-    var videoName = ((prefs.addUserInfo ? user + " - " : "") + title).replace(/[:]/g, "").replace(/[\\\/]/g, "-").replace(/[?]/g, "").replace(/[*]/g, "^").replace(/["]/g, "'").replace(/[<]/g, "[").replace(/[>]/g, "]").replace(/[|]/g, "-");
+    var videoName = ((prefs.addUserInfo ? user + " - " : "") + title).replace(/[:\?\¿]/g, "")
+                                                                     .replace(/[\\\/]/g, "-")
+                                                                     .replace(/[*]/g, "^")
+                                                                     .replace(/[\"]/g, "'")
+                                                                     .replace(/[<]/g, "[")
+                                                                     .replace(/[>]/g, "]")
+                                                                     .replace(/[|]/g, "-");
     var audioName = videoName;
     var root, audioPath = [], videoPath = [];
     var iFile, oFile;
